adb6205349c4c00e2f05ac2046be5c56ddcc5655
l11040 <113540683+l11040@users.noreply.github.com>
2024-05-16 20:24:14 +0900

    Update : PlayerController Input System 연동

--- Assets/Script/Entity/Controller/PlayerController.cs
+++ Assets/Script/Entity/Controller/PlayerController.cs
@@ -1,3 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
 public class PlayerController : EntityController
 {
+    public void OnMove(InputValue value)
+    {
+        Debug.Log("움직임");
+    }
+
+    public void OnLook(InputValue value)
+    {
+        Debug.Log("마우스 움직임");
+    }
+
+    public void OnDash(InputValue value)
+    {
+        Debug.Log("Shift");
+    }
+
+    public void OnAttack(InputValue value)
+    {
+        Debug.Log("공격");
+    }
 }
