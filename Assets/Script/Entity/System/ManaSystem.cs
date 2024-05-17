0bd587a9ca7cc528e8a8ad46b6fa48b71ba0ee44
lsy1304 <lsy130425@gmail.com>
2024-05-17 14:54:07 +0900

    Update : ManaSystem 수정

--- Assets/Script/Entity/System/ManaSystem.cs
+++ Assets/Script/Entity/System/ManaSystem.cs
@@ -3,5 +3,45 @@ using UnityEngine;
 
 public class ManaSystem : MonoBehaviour
 {
-    
+    private CharacterStatHandler _statHandler;
+
+    public float CurrentMP {  get; private set; }
+    public event Action OnUsed;
+    public event Action OnFilled;
+    public float MaxMP => _statHandler.CurrentStat.MaxMP;
+
+    private void Awake()
+    {
+        _statHandler = GetComponent<CharacterStatHandler>();
+    }
+
+    private void Start()
+    {
+        CurrentMP = MaxMP;
+    }
+
+    public void ChangeMP(float change)
+    {
+        CurrentMP += change;
+        CurrentMP = Mathf.Clamp(CurrentMP, 0f, MaxMP);
+
+        if(change <= 0f)
+        {
+            UseMana();
+        }
+        else
+        {
+            FillMana();
+        }
+    }
+
+    private void FillMana()
+    {
+        OnFilled?.Invoke();
+    }
+
+    private void UseMana()
+    {
+        OnUsed?.Invoke();
+    }
 }
