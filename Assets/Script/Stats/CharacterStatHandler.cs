277bbbf4c2a9b5cbd7c9247f66f8bf462648fd43
welephant3 <tlswjsrltn@naver.com>
2024-05-17 14:21:53 +0900

    Add : Stat, CharactersStatHandler 추가

---
+++ Assets/Script/Stats/CharacterStatHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class CharacterStatHandler : MonoBehaviour
+{
+    public Stat CurrentStat { get; private set; }
+    [SerializeField] private Stat _baseStat;
+    private List<Stat> _statModifiers = new List<Stat>();    
+
+    private void Awake()
+    {
+        UpdateStat();
+    }
+
+    public void UpdateStat()
+    {
+        CurrentStat = _baseStat.DeepCopy();
+        foreach (Stat stat in _statModifiers)
+        {
+            switch(stat.ChangeType)
+            { 
+                case StatChangeType.Add:
+                    break;
+                case StatChangeType.Multiple:
+                    break;
+                case StatChangeType.Override:
+                    break;
+
+            }
+        }
+    }
+
+    public void AddStat(Stat stat)
+    {
+        _statModifiers.Add(stat);
+    }
+}
---
+++ Assets/Script/Stats/Stat.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+public enum StatChangeType
+{
+    Add,
+    Multiple,
+    Override
+}
+
+[Serializable]
+public class Stat
+{ 
+    public StatChangeType ChangeType;
+    [Range(1, 100)] public int MaxHP;
+    [Range(1, 100)] public int MaxMP;
+    [Range(1f, 20f)] public float Speed;
+    [Range(1f, 5f)] public float DashCoolTime;
+    public AttackSO AttackData;
+
+    public virtual Stat DeepCopy()
+    {
+        Stat newStat = (Stat)Activator.CreateInstance(GetType());
+
+        newStat.ChangeType = this.ChangeType;
+        newStat.MaxHP = this.MaxHP;
+        newStat.MaxMP = this.MaxMP;
+        newStat.Speed = this.Speed;
+        newStat.DashCoolTime = this.DashCoolTime;
+
+        if (this.AttackData != null)
+        {
+            newStat.AttackData = this.AttackData.DeepCopy();
+        }
+
+        return newStat;
+    }
+}
