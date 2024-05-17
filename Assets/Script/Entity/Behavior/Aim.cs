7fa9ade42fa6dc845ab49f084152e50fb0b7fcbb
welephant3 <tlswjsrltn@naver.com>
2024-05-17 20:11:43 +0900

    Add : Aim 추가

---
+++ Assets/Script/Entity/Behavior/Aim.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{   
+    [SerializeField] private Transform ArmPivot;
+
+    private EntityController _controller;
+
+    private void Awake()
+    {
+        _controller = GetComponent<EntityController>();
+        _controller.OnLookEvent += OnAim;
+    }   
+
+    private void OnAim(Vector2 direction)
+    {
+        float rotZ = MathF.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;              
+        ArmPivot.rotation = Quaternion.Euler(0, 0, rotZ);
+    }
+}
