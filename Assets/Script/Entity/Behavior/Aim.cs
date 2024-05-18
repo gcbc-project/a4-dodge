b011dc29435cd42dd4b098f5394714d3b188cd8b
l11040 <113540683+l11040@users.noreply.github.com>
2024-05-19 00:02:51 +0900

    Update : Aim 세팅

--- Assets/Script/Entity/Behavior/Aim.cs
+++ Assets/Script/Entity/Behavior/Aim.cs
@@ -2,8 +2,9 @@ using System;
 using UnityEngine;
 
 public class Aim : MonoBehaviour
-{   
+{
     [SerializeField] private Transform _armPivot;
+    [SerializeField] private Transform _entityPivot;
 
     private EntityController _controller;
 
@@ -11,11 +12,14 @@ public class Aim : MonoBehaviour
     {
         _controller = GetComponent<EntityController>();
         _controller.OnLookEvent += OnAim;
-    }   
+    }
 
     private void OnAim(Vector2 direction)
     {
-        float rotZ = MathF.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;              
+        float rotZ = MathF.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Vector2 scale = _entityPivot.localScale;
+        scale.x = Mathf.Abs(rotZ) > 90f ? -1f : 1f;
+        _entityPivot.localScale = scale;
         _armPivot.rotation = Quaternion.Euler(0, 0, rotZ);
     }
 }
