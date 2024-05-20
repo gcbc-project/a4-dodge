1a6a3b8a4f660445685a1fc6455480183ad150f4
lsy1304 <lsy130425@gmail.com>
2024-05-20 14:40:00 +0900

    Update : ProjectileController 수정

--- Assets/Script/Other/ProjectileController.cs
+++ Assets/Script/Other/ProjectileController.cs
@@ -1,18 +1,71 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ProjectileController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private LayerMask _levelCollisionLayer;
+
+    private RangeAttackSO _attackData;
+    private Vector2 _direction;
+    private bool _isReady;
+    private Rigidbody2D _rgbd;
+
+    private void Awake()
     {
-        
+        _rgbd = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!_isReady)
+        {
+            return;
+        }
+
+        _rgbd.velocity = _direction * _attackData.ProjectileSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (IsLayerMatched(_levelCollisionLayer.value, collision.gameObject.layer))
+        {
+            Vector2 destroyPosition = collision.ClosestPoint(transform.position) - _direction * 2f;
+            DestroyProjectile();
+        }
+        else if (IsLayerMatched(_attackData.Target.value, collision.gameObject.layer))
+        {
+            HealthSystem healthSystem = collision.GetComponent<HealthSystem>();
+            if (healthSystem != null)
+            {
+                bool isAttackApplied = healthSystem.ChangeHP(-_attackData.ATK);
+            }
+            DestroyProjectile();
+        }
+    }
+
+    public void Init(Vector2 dirction, RangeAttackSO attackData)
+    {
+        this._attackData = attackData;
+        this._direction = dirction;
+
+        UpdateProjectileSprite();
+
+        transform.right = this._direction;
+        _isReady = true;
+    }
+
+    private void DestroyProjectile()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private bool IsLayerMatched(int layerMask, int objectLayer)
+    {
+        return layerMask == (layerMask | (1 << objectLayer));
+    }
+
+    private void UpdateProjectileSprite()
+    {
+        transform.localScale = Vector3.one * _attackData.ProjectileSize;
     }
 }
