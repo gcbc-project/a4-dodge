e363245db280db4b35e30d0ef8132e5c5b4f4984
lsy1304 <lsy130425@gmail.com>
2024-05-20 19:05:45 +0900

    Update
    Player에 StatusUIController 추가
    UI 연결

--- Assets/Script/Entity/Controller/StateUIController.cs
+++ Assets/Script/Entity/Controller/StateUIController.cs
@@ -1,9 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StateUIController : MonoBehaviour
 {
+    [SerializeField] private Image _healthUI;
+    [SerializeField] private Image _ManaUI;
+    [SerializeField] private Image _DashUI;
     // Start is called before the first frame update
     void Start()
     {
