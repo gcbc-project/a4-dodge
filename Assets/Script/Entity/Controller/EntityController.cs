d45512298acec34769e4cd3abdc20f7b993019fb
limjh0222 <97434772+limjh0222@users.noreply.github.com>
2024-05-20 17:06:23 +0900

    Fix : EntityController
    - 간격 맞춤

--- Assets/Script/Entity/Controller/EntityController.cs
+++ Assets/Script/Entity/Controller/EntityController.cs
@@ -1,36 +1,59 @@
 using System;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 
 public abstract class EntityController : MonoBehaviour
 {
-  public event Action<Vector2> OnMoveEvent;
-  public event Action<Vector2> OnLookEvent;
-  public event Action OnDashEvent;
-  public event Action<AttackSO> OnAttackEvent;
+    public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnLookEvent;
+    public event Action OnDashEvent;
+    public event Action<AttackSO> OnAttackEvent;
 
     protected CharacterStatHandler _statHandler;
+
+    protected bool isAttacking { get; set; }
+    private float timeSinceLastAttack = float.MaxValue;
 
     protected virtual void Awake()
     {
         _statHandler = GetComponent<CharacterStatHandler>();
     }
+
+    private void Update()
+    {
+        HandleAttackDelay();
+    }
+
+    private void HandleAttackDelay()
+    {
+        if (timeSinceLastAttack < _statHandler.CurrentStat.AttackData.CoolTime)
+        {
+            timeSinceLastAttack += Time.deltaTime;
+        }
+        else if (isAttacking && timeSinceLastAttack >= _statHandler.CurrentStat.AttackData.CoolTime)
+        {
+            timeSinceLastAttack = 0f;
+            CallAttackEvent(_statHandler.CurrentStat.AttackData);
+        }
+    }
+
     public void CallMoveEvent(Vector2 direction)
-  {
-    OnMoveEvent?.Invoke(direction);
-  }
+    {
+        OnMoveEvent?.Invoke(direction);
+    }
 
-  public void CallLookEvent(Vector2 direction)
-  {
-    OnLookEvent?.Invoke(direction);
-  }
+    public void CallLookEvent(Vector2 direction)
+    {
+        OnLookEvent?.Invoke(direction);
+    }
 
-  public void CallDashEvent()
-  {
-    OnDashEvent?.Invoke();
-  }
+    public void CallDashEvent()
+    {
+        OnDashEvent?.Invoke();
+    }
 
-  public void CallAttackEvent(AttackSO attackData)
-  {
-    OnAttackEvent?.Invoke(attackData);
-  }
+    public void CallAttackEvent(AttackSO attackData)
+    {
+        OnAttackEvent?.Invoke(attackData);
+    }
 }
