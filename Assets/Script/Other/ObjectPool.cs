db7fbf84e84391392c47dc7f58639eaa55d65595
lsy1304 <lsy130425@gmail.com>
2024-05-20 11:08:13 +0900

    Update : ObjectPool  수정

--- Assets/Script/Other/ObjectPool.cs
+++ Assets/Script/Other/ObjectPool.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ObjectPool : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public class Pool
     {
-        
+        public string Tag;
+        public GameObject Prefab;
+        public int Size;
     }
 
-    // Update is called once per frame
-    void Update()
+    public List<Pool> Pools = new List<Pool>();
+    public Dictionary<string, Queue<GameObject>> PoolDictionary;
+
+    private void Awake()
     {
-        
+        PoolDictionary = new Dictionary<string, Queue<GameObject>>();
+        foreach(var pool in Pools)
+        {
+            Queue<GameObject> queue = new Queue<GameObject>();
+            for (int i = 0; i < pool.Size; i++) 
+            {
+                GameObject go = Instantiate(pool.Prefab, transform);
+                go.SetActive(false);
+                queue.Enqueue(go);
+            }
+            PoolDictionary.Add(pool.Tag, queue);
+        }
+    }
+
+    public GameObject SpawnFromPool(string tag)
+    {
+        if (!PoolDictionary.ContainsKey(tag)) return null;
+
+        GameObject go = PoolDictionary[tag].Dequeue();
+        PoolDictionary[tag].Enqueue(go);
+        go.SetActive(true);
+        return go;
     }
 }
