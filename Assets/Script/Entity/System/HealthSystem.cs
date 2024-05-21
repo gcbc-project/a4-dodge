d0ebd777a31eaa4a98106c8bc577b5d3067908f3
lsy1304 <lsy130425@gmail.com>
2024-05-21 10:51:10 +0900

    Update : HealthSystem 수정
    ChangeHP 내용물 수정
    주석 제거

--- Assets/Script/Entity/System/HealthSystem.cs
+++ Assets/Script/Entity/System/HealthSystem.cs
@@ -3,19 +3,19 @@ using UnityEngine;
 
 public class HealthSystem : MonoBehaviour
 {
-    [SerializeField] private float _healthChangeDelay = 0.5f; // 무적 시간 (피격 시 데미지를 무시하는 구간)
+    [SerializeField] private float _healthChangeDelay = 0.5f;
     
-    public float CurrnetHP { get; private set; } // 현재 체력
-    public event Action OnDamageEvent; // 피격 이벤트
-    public event Action OnHealEvent; // 치유 이벤트
-    public event Action OnDeathEvent; // 사망 이벤트
-    public event Action OnInvincibilityEndEvent; // 무적 해제 이벤트
+    public float CurrentHP { get; private set; }
+    public event Action OnDamageEvent;
+    public event Action OnHealEvent;
+    public event Action OnDeathEvent;
+    public event Action OnInvincibilityEndEvent;
     public event Action<float> OnHPChangeEvent;
-    public float MaxHP => _statHandler.CurrentStat.MaxHP; // 최대 채력 = 현재 스탯의 최대 채력
+    public float MaxHP => _statHandler.CurrentStat.MaxHP;
 
-    private CharacterStatHandler _statHandler; // 현재 스탯 수치를 가져올 변수
-    private float _timeSinceLastChange = float.MaxValue; // 초기 마지막 피격 시간을 최대치로 설정 (바로 때릴 수 있게)
-    private bool _isAttacked = false; // 현재 피격 상태 초기화
+    private CharacterStatHandler _statHandler;
+    private float _timeSinceLastChange = float.MaxValue;
+    private bool _isAttacked = false;
  
     private void Awake()
     {
@@ -24,59 +24,47 @@ public class HealthSystem : MonoBehaviour
 
     void Start()
     {
-        CurrnetHP = MaxHP; // 첫 기동 시 현재 체력을 스탯의 최대 체력으로 초기화
+        CurrentHP = MaxHP;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_isAttacked && _timeSinceLastChange < _healthChangeDelay) // 공격받은 상태이고 딜레이 시간이 경과하지 않은 경우
+        if (_isAttacked && _timeSinceLastChange < _healthChangeDelay)
         {
-            _timeSinceLastChange += Time.deltaTime; // 경과 시간 업데이트
-            if (_timeSinceLastChange >= _healthChangeDelay) // 딜레이 시간이 경과하면
+            _timeSinceLastChange += Time.deltaTime;
+            if (_timeSinceLastChange >= _healthChangeDelay)
             {
-                InvincibilityEnd(); // 무적 종료 이벤트 호출
-                _isAttacked = false; // 공격받은 상태 해제
+                InvincibilityEnd();
+                _isAttacked = false;
             }
         }
-
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            Debug.Log($"데미지 + {_timeSinceLastChange}");
-            ChangeHP(-10f);
-        }
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            Debug.Log($"치유 + { _timeSinceLastChange}");
-            ChangeHP(10f);
-        }
     }
 
-    public bool ChangeHP(float change) // 체력 변경 메서드
+    public bool ChangeHP(float change)
     {
-        if (_isAttacked && _timeSinceLastChange < _healthChangeDelay) return false; // 데미지일 경우 딜레이 체크
-        _timeSinceLastChange = 0f;
-        CurrnetHP += change; // 현재 체력 변경
-        CurrnetHP = Mathf.Clamp(CurrnetHP, 0f, MaxHP); // 현재 체력을 0과 최대 체력 사이로 제한
-        if (CurrnetHP <= 0f) // 체력이 0 이하이면?
+        if (change > 0f || _timeSinceLastChange >= _healthChangeDelay)
         {
-            Death(); // 사망 이벤트 호출
-            return true; // 체력 변경 성공
+            if(change > 0f) Heal();
+            else if(_timeSinceLastChange >= _healthChangeDelay) _timeSinceLastChange = 0f;
+            CurrentHP += change;
+            CurrentHP = Mathf.Clamp(CurrentHP, 0f, MaxHP);
+
+            if (change <= 0f)
+            {
+                Damage();
+                _isAttacked = true;
+            }
+            else if (CurrentHP <= 0f)
+            {
+                Death();
+                return true;
+            }
+            OnHPChangeEvent?.Invoke(CurrentHP);
         }
-        if (change > 0f) // 체력이 회복되는 경우
-        {
-            Heal(); // 치유 이벤트 호출
-        }
-        else if (change < 0f) // 체력이 감소되는 경우
-        {
-            Damage(); // 데미지 이벤트 호출
-            _isAttacked = true; // 공격받은 상태로 설정
-        }
-        OnHPChangeEvent?.Invoke(CurrnetHP);
-        return true; // 체력 변경 성공
+        return false;
     }
 
-    // 이벤트 캡슐화
     private void Death()
     {
         OnDeathEvent?.Invoke();
